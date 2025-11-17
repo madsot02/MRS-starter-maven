73ca0b0242b50786ea670931e86d570fe1404bcc
MADS-THINKPAD\madst <madsot01@easv365.dk>
2025-11-17 10:29:28 +0100

    sidste update foer aendring

---
+++ src/main/java/dk/easv/mrs/DAL/db/MOVIEDAO_DB.java
@@ -0,0 +1,44 @@
+package dk.easv.mrs.DAL.db;
+
+import dk.easv.mrs.BE.Movie;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MOVIEDAO_DB {
+
+    public List<Movie> getAllMovies() throws Exception {
+        ArrayList<Movie> allMovies = new ArrayList<>();
+
+        DBConnector databaseConnector = new DBConnector();
+        try (Connection conn = databaseConnector.getConnection();
+             Statement stmt = conn.createStatement())
+        {
+            String sql = "SELECT * FROM dbo.Movie;";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            // Loop through rows from the database result set
+            while (rs.next()) {
+
+                //Map DB row to Movie object
+                int id = rs.getInt("Id");
+                String title = rs.getString("Title");
+                int year = rs.getInt("year");
+
+                Movie movie = new Movie(id, year, title);
+                allMovies.add(movie);
+            }
+            return allMovies;
+
+        }
+        catch (SQLException ex)
+        {
+            ex.printStackTrace();
+            throw new Exception("Could not get movies from database", ex);
+        }
+    }
+}
